6632f0bea18acd5a3eb118a7196e41d174bf1bcd
Nick Holt <nick.holt@btinternet.com>
2025-06-03 17:15:18 +0100

    Adding sorting.

--- src/main/java/com/tn/query/jpa/QueryableRepository.java
+++ src/main/java/com/tn/query/jpa/QueryableRepository.java
@@ -2,10 +2,13 @@ package com.tn.query.jpa;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 public interface QueryableRepository<T>
 {
   Iterable<T> findWhere(String query);
+
+  Iterable<T> findWhere(String query, Sort sort);
 
   Page<T> findWhere(String query, PageRequest pageRequest);
 }
